d404ba5dcfddba19e186d6e51c6579d0d806414a
A <shutdown>
2018-10-16 20:45:33 -0500

    se agregó una clase circulo

---
+++ Project3/CCirculo.h
@@ -0,0 +1,55 @@
+#pragma once
+using namespace System;
+class Circulo
+{
+private:
+	int x, y, radio, dx, dy;
+public:
+	Circulo(void);
+	~Circulo(void);
+	Circulo(int px, int py,int pr);
+	void borrar(Drawing::Graphics ^gr);
+	void mover(Drawing::Graphics ^gr);
+	void dibujar(Drawing::Graphics ^gr);
+};
+Circulo::Circulo()
+{
+	x = y = dx = dy = radio = 0;
+}
+Circulo::~Circulo()
+{
+
+}
+Circulo::Circulo(int px, int py, int pr)
+{
+	x = px;
+	y = py;
+	radio = pr;
+	dx = 5;
+	dy = 5;
+}
+void Circulo::borrar(Drawing::Graphics ^gr)
+{
+	gr->DrawEllipse(gcnew Drawing::Pen(Drawing::Color::White), x, y, radio, radio);
+
+}
+void Circulo::dibujar(Drawing::Graphics ^gr)
+{
+	gr->DrawEllipse(gcnew Drawing::Pen(Drawing::Color::Red), x, y, radio, radio);
+
+}
+void Circulo::mover(Drawing::Graphics ^gr)
+{
+	borrar(gr);
+	if (x+dx<1||x+dx+radio<gr->VisibleClipBounds.Width)
+	{
+		dx *= -1;
+	}	
+	if (y + dy<1 || y + dy + radio<gr->VisibleClipBounds.Height)
+	{
+		dy *= -1;
+	}
+	x += dx;
+	y += dy;
+	dibujar(gr);
+}
